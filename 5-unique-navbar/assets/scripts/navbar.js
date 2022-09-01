e885432fda855f7dbf09057051a8d6b0039c700e
Krishan Prajapat <111554369+krishanprajapat23@users.noreply.github.com>
2022-09-01 22:48:52 +0530

    updating project 5 fancy navbar page

---
+++ 5-unique-navbar/assets/scripts/navbar.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll(".navbar-link");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        links.forEach((link) => {
+            link.className = "navbar-link";
+        });
+        link.classList.add("change");
+
+        link.previousElementSibling.classList.add("prevElementChange");
+        link.nextElementSibling.classList.add("nextElementChange");
+    });
+});
